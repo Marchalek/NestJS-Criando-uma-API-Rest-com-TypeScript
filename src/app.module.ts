b65193a13a423a704b01624a0dcadfb963b4400e
Marchalek <marchalek@hotmail.com>
2021-07-26 16:38:59 -0300

    CURSO TERMINADO

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,17 @@ import { LivrosController } from './livrros.controller';
 import { LivrosService } from './livros.service';
 import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 import { Livro } from './livro.model';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'Raiodante@12',
+      username: process.env.USUARIO_BANCO_DADOS,
+      password: process.env.SENHA_BANCO_DADOS,
       database: 'livraria',
       autoLoadModels: true,
       synchronize: true
